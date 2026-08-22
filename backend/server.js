a8a5e1396b68320d596745c9e64ad7b65ea641bc
lakshayaneja2@gmail.com <lakshayaneja2@gmail.com>
2026-08-22 23:52:21 +0530

    fix: API on Render

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,27 @@
 const express = require('express')
-// const mongoose = require('mongoose')
 const cors = require('cors')
-const dotenv = require('dotenv')
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/auth');
 const transactionRoutes = require('./routes/transactions');
-
-dotenv.config();
 
 const app = express();
 
 // Connect to DB
 connectDB();
 
-
-// MiddleWare
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-//Routes
-app.use('/api/auth',authRoutes);
+// Routes
+app.use('/api/auth', authRoutes);
 app.use('/api/transactions', transactionRoutes);
 
-//Test Route
-app.get('/', (req,res) => {
-    res.json({message : 'Finance Tracker API is running'});
+// Test Route
+app.get('/', (req, res) => {
+  res.json({ message: 'Finance Tracker API is running' });
 });
 
-app.listen(process.env.PORT, () => {
-        console.log(`Server running on port ${process.env.PORT}`);
-    });
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server running on port ${process.env.PORT || 5000}`);
+});
